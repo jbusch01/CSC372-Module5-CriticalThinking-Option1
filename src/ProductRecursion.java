031d5effd53d097a10ffc66802374c899c7db80c
jbusch01 <jonrbusch@gmail.com>
2025-05-17 08:55:37 -0600

    First Commit

---
+++ src/ProductRecursion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ProductRecursion {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        double num1;
+        double num2;
+        double num3;
+        double num4;
+        double num5;
+        double total;
+
+        System.out.println("Please enter 5 numbers of your choosing:\n");
+        num1 = scnr.nextDouble();
+        num2 = scnr.nextDouble();
+        num3 = scnr.nextDouble();
+        num4 = scnr.nextDouble();
+        num5 = scnr.nextDouble();
+
+        for ()
+    }
+}
