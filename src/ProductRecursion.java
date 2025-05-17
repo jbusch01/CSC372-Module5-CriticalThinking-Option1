91989538945a2aa3c68e1044818dfa91181e5e6d
jbusch01 <jonrbusch@gmail.com>
2025-05-17 10:05:35 -0600

    Added user input validation

--- src/ProductRecursion.java
+++ src/ProductRecursion.java
@@ -5,22 +5,34 @@ class ProductRecursion {
         Scanner scnr = new Scanner(System.in);
         int[] numbers = new int[5];
         
-        System.out.println("Enter 5 whole numbers you wish to multiply:");
+        System.out.println("Enter 5 whole numbers to multiply:");
 
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println("Number " + (i + 1) + ": ");
-            numbers[i] = scnr.nextInt();
+            while (true) {
+                System.out.println("Number " + (i + 1) + ": ");
+                if (scnr.hasNextInt()) {
+                    numbers[i] = scnr.nextInt();
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter a whole number (no decimals or commas).");
+                    scnr.next();
+                }
+            }
         }
 
         int product = multiply(numbers, 0);
         System.out.println("The total of all numbers multiplied together is: " + product);
 
+        scnr.close();
+
     }
+    
 
     public static int multiply(int[] numbers, int index) {
         if (index == numbers.length) {
             return 1;
         }
+
         return numbers[index] * multiply(numbers, index + 1);
     }
 }
